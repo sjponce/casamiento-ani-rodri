bf7ad750da0d962db0381d56a4b138813aadaf0b
sjponce <santiago.j.ponce@gmail.com>
2023-05-21 00:15:35 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CountDownComponent } from './count-down/count-down.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CountDownComponent
+    CountDownComponent,
+    CalendarComponent,
   ],
   imports: [
     BrowserModule,
